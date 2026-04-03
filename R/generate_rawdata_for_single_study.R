5a750b7d86b072b02ddc4b74e6d309178235fe65
zdz2101 <zelos.zhu@atorusresearch.com>
2026-04-03 09:56:47 -0700

    revert back to whats on dev

--- R/generate_rawdata_for_single_study.R
+++ R/generate_rawdata_for_single_study.R
@@ -72,7 +72,7 @@ generate_snapshots_from_combined_specs <- function(SnapshotCount,
 
   ae_count <- subject_count * 3
   pd_count <- subject_count * 3
-  sdrgcomp_count <- subject_count
+  sdrgcomp_count <- ceiling(subject_count / 10)
   studcomp_count <- ceiling(subject_count / 10)
   consents_count <- ceiling(subject_count / 75)
   death_count <- ceiling(subject_count / 85)
